d48f5d70755b4b34cfa707e1c020b6a7357f93ac
Preetesh Verma <verma.preetesh21@gmail.com>
2020-09-25 13:16:08 +0530

    added corrections for corner cases

--- app.js
+++ app.js
@@ -41,10 +41,7 @@ function requestUserRepos(username) {
         while (root.firstChild) {
             root.removeChild(root.firstChild);
         }
-        // Loop over each object in data array
-        for (let i in data) {
-
-            // Get the ul with id of of userRepos
+        if (data.message === "Not Found") {
             let ul = document.getElementById('userRepos');
 
             // Create variable that will create li's to be added to ul
@@ -52,19 +49,39 @@ function requestUserRepos(username) {
 
             // Add Bootstrap list item class to each li
             li.classList.add('list-group-item')
-
-            // Create the html markup for each li
+                // Create the html markup for each li
             li.innerHTML = (`
+                <p><strong>No account exists with username:</strong> ${username}</p>`);
+            // Append each li to the ul
+            ul.appendChild(li);
+        } else {
+
+            // Get the ul with id of of userRepos
+            let ul = document.getElementById('userRepos');
+            let p = document.createElement('p');
+            p.innerHTML = (`<p><strong>Number of Public Repos:${data.length}</p>`)
+            ul.appendChild(p);
+            // Loop over each object in data array
+            for (let i in data) {
+                // Create variable that will create li's to be added to ul
+                let li = document.createElement('li');
+
+                // Add Bootstrap list item class to each li
+                li.classList.add('list-group-item')
+
+                // Create the html markup for each li
+                li.innerHTML = (`
                 <p><strong>Repo:</strong> ${data[i].name}</p>
                 <p><strong>Description:</strong> ${data[i].description}</p>
                 <p><strong>URL:</strong> <a href="${data[i].html_url}">${data[i].html_url}</a></p>
             `);
 
-            // Append each li to the ul
-            ul.appendChild(li);
+                // Append each li to the ul
+                ul.appendChild(li);
+
+            }
 
         }
-
     }
 
     // Send the request to the server
